1392ec42ddcb3b955b02f05a9dba5ace2a634ef8
Ali Raza <alirazacui31@gmail.com>
2025-05-16 09:56:09 +0500

    added some of backedn files
    added files like utils and its components

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+connectDB();
+
+// Initialize Express
+const app = express();
+app.use(express.json()); // Parse JSON data
+
+// Test endpoint
+app.get("/", (req, res) => {
+  res.send("Hello from Student Task Tracker API!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
